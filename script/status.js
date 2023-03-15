eda57a08c73433a94e779439adfef59a3c96209d
landycodes <landryandrewsk8@gmail.com>
2023-03-14 17:48:24 -0700

    status toggle & dynamic adjustments

--- script/status.js
+++ script/status.js
@@ -1,8 +1,22 @@
+//toggle switch for brake/tread
+//timeout set to transtion length
+
+//set initial color on tread
+$("#tread").css("color", "black");
 $("#toggle").click(() => {
   if ($("#toggle").prop("checked")) {
-    $("#slider").addClass("slide-right");
+    $(".slider").removeClass("slide-left");
+    $(".slider").addClass("slide-right");
+    $("#tread").css("color", "white");
+    setTimeout(() => {
+      $("#brake").css("color", "black");
+    }, 500);
   } else {
-    $("#slider").removeClass("slide-right");
-    $("#slider").addClass("slide-left");
+    $(".slider").removeClass("slide-right");
+    $(".slider").addClass("slide-left");
+    $("#brake").css("color", "white");
+    setTimeout(() => {
+      $("#tread").css("color", "black");
+    }, 500);
   }
 });
