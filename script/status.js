452a749da6d0f03afa46d56d19d414c3b92e2708
landycodes <landryandrewsk8@gmail.com>
2023-03-17 13:41:15 -0700

    input boxes made for status

--- script/status.js
+++ script/status.js
@@ -1,12 +1,20 @@
-//set initial color on tread slider text
+//set initial color on tread slider text and measurment type
 $("#tread").css("color", "black");
+$(".measurement").text("/32 inch");
+
 //hide input box on page load
-$(".stat-val").css("display", "none");
+// $(".tread-val").css("display", "none");
 
 //toggle switch for brake/tread
 //timeout set to transtion length
 $("#toggle").click(() => {
+  //reset edit/input on toggle
+  $(".tread-val").css("display", "none");
+  $("#edit-stat").text("Edit");
+
+  //checked = brake stats, else = tread stats
   if ($("#toggle").prop("checked")) {
+    $(".measurement").text("mm");
     $(".slider").removeClass("slide-left");
     $(".slider").addClass("slide-right");
     $("#tread").css("color", "white");
@@ -14,6 +22,7 @@ $("#toggle").click(() => {
       $("#brake").css("color", "black");
     }, 500);
   } else {
+    $(".measurement").text("/32 inch");
     $(".slider").removeClass("slide-right");
     $(".slider").addClass("slide-left");
     $("#brake").css("color", "white");
@@ -24,6 +33,20 @@ $("#toggle").click(() => {
 });
 
 $("#edit-stat").click(() => {
-  $(".stat-val").css("display", "block");
-  $("#edit-stat").text("Save");
+  if ($("#edit-stat").text() === "Edit") {
+    if ($("#toggle").prop("checked")) {
+      $(".brake-val").css("display", "block");
+    } else {
+      $(".tread-val").css("display", "block");
+    }
+    $("#edit-stat").text("Save");
+  } else if ($("#edit-stat").text() === "Save") {
+    //if values arent empty save them to local storage
+    if ($("#toggle").prop("checked")) {
+      $(".brake-val").css("display", "none");
+    } else {
+      $(".tread-val").css("display", "none");
+    }
+    $("#edit-stat").text("Edit");
+  }
 });
